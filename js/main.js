a7d3a30a3f644dd7a25a00602ecbeeb0e2df5f15
CosmicPixel <CosmicPixel@github.com>
2020-08-23 16:49:33 -0400

    Modified json function data

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-$(document).ready(()=>{
+$(document).ready(()=>{ 
 
-    $("#button").click(()=>{
-        let userInput=$("#Search").val()
-        alert(userInput)
 
-    $.ajax({
-        var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=$(keyword)api_key=oECEbvJh3Q3KBv6UiSycVPH7HgVlEG5B&limit=20");
-        xhr.done(function(data) { console.log("success got data", data); });
+    var userInput= document.getElementById("input").value
+    console.log(userInput)
+    var giphyApiKey = oECEbvJh3Q3KBv6UiSycVPH7HgVlEG5B
+    var giphyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}'
 
+
+    fetch(giphyApiUrl).then(function(res) {
+        return res.json()
     })
+    .then(function(json) {
+        console.log(json.data[0].images.fixed_height.url)
+        var imgPath =json.data[0].images.fixed_height.url
+        var img = document.createElement("img")
+        img.setAttribute("src", imgPath)
+        document.body.appendChild(img)
+
+        })
+            
+        
+    }).catch(function(error) {
+        console.log(error.message)
+
     })
 })
+
+
